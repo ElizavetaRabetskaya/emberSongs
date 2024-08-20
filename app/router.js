994c8dbe49f55f69a07d164b05760ba1067c47d7
Elizaveta Rabetskaya <elizaveta_rabetskaya@epam.com>
2024-08-20 23:28:58 +0400

    routes2

--- app/router.js
+++ app/router.js
@@ -5,8 +5,20 @@ export default class Router extends EmberRouter {
   location = config.locationType;
   rootURL = config.rootURL;
 }
+// Здесь :band_id является динамическим сегментом URL.
+// Это значит, что при переходе к такому URL,
+//  как /bands/1, значение 1 будет передано в маршрут как часть params.
 
+// model For - это действительно отличный метод, который
+// извлекает модель родительского маршрута, который уже был
+// активирован.
+
+// полный переход "сверху вниз"
+// запускается только при начальной загрузке приложения.
 Router.map(function () {
-  this.route('bands');
-  this.route('songs');
+  this.route('bands', function () {
+    this.route('band', { path: ':id' }, function () {
+      this.route('songs');
+    });
+  });
 });
