8357ebbef1cafbc4cb28fdc24606db2d970cb1bf
Elizaveta Rabetskaya <elizaveta_rabetskaya@epam.com>
2024-08-21 18:38:52 +0400

    services

--- app/controllers/bands/new.js
+++ app/controllers/bands/new.js
@@ -2,9 +2,14 @@ import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import { dasherize } from '@ember/string';
 import { tracked } from '@glimmer/tracking';
-import { Band } from 'rarwe/routes/bands';
+// import { Band } from 'rarwe/routes/bands';
+import Band from 'rarwe/models/band';
+import { service } from '@ember/service';
 
 export default class BandsNewController extends Controller {
+  @service catalog;
+  @service router;
+
   @tracked name;
   get hasNoName() {
     return !this.name;
@@ -15,6 +20,8 @@ export default class BandsNewController extends Controller {
   }
   @action
   saveBand() {
-    new Band({ name: this.name, id: dasherize(this.name) });
+    let band = new Band({ name: this.name, id: dasherize(this.name) });
+    this.catalog.add('band', band);
+    this.router.transitionTo('bands.band.songs', band.id);
   }
 }
