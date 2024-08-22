bb8d3326a1b806e40a1277edd5837118cb319230
Elizaveta Rabetskaya <elizaveta_rabetskaya@epam.com>
2024-08-22 16:34:06 +0400

    routeWillChange

--- app/controllers/bands/new.js
+++ app/controllers/bands/new.js
@@ -15,6 +15,31 @@ export default class BandsNewController extends Controller {
   get hasNoName() {
     return !this.name;
   }
+
+  constructor() {
+    super(...arguments);
+    this.router.on('routeWillChange', (transition) => {
+      if (transition.isAborted) {
+        return;
+      }
+
+      if (this.confirmedLeave) {
+        return;
+      }
+
+      if (transition.from.name === 'bands.new') {
+        if (this.name) {
+          let leave = window.confirm('You have unsaved changes. Are you sure?');
+          if (leave) {
+            this.confirmedLeave = true;
+          } else {
+            transition.abort();
+          }
+        }
+      }
+    });
+  }
+
   @action
   updateName(event) {
     this.name = event.target.value;
@@ -47,6 +72,7 @@ export default class BandsNewController extends Controller {
     // this.catalog.add('band', record);
     // this.router.transitionTo('bands.band.songs', id);
     let band = await this.catalog.create('band', { name: this.name });
+    this.confirmedLeave = true;
     this.router.transitionTo('bands.band.songs', band.id);
   }
 }
