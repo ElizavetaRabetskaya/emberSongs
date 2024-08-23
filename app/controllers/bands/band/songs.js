3175282ec15fa9407cf37df70b1d9b5a95b5f5fe
Elizaveta Rabetskaya <elizaveta_rabetskaya@epam.com>
2024-08-23 11:43:28 +0400

    desc sort

--- app/controllers/bands/band/songs.js
+++ app/controllers/bands/band/songs.js
@@ -8,12 +8,32 @@ import { service } from '@ember/service';
 export default class BandsBandSongsController extends Controller {
   @tracked showAddSong = true;
   @tracked title = '';
+  @tracked sortBy = 'title';
 
   get hasNoTitle() {
     return !this.title;
   }
 
   @service catalog;
+
+  get sortedSongs() {
+    let sortBy = this.sortBy;
+    let isDescendingSort = false;
+    if (sortBy.charAt(0) === '-') {
+      sortBy = this.sortBy.slice(1);
+      isDescendingSort = true;
+    }
+
+    return [...this.model.songs].sort((song1, song2) => {
+      if (song1[sortBy] < song2[sortBy]) {
+        return isDescendingSort ? 1 : -1;
+      }
+      if (song1[sortBy] > song2[sortBy]) {
+        return isDescendingSort ? -1 : 1;
+      }
+      return 0;
+    });
+  }
 
   @action
   async updateRating(song, rating) {
